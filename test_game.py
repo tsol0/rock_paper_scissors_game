05c1051bd086f1963fd9b8e75a1d129c45b05ebd
Tsolo Khambule <tskhamb022@student.wethinkcode.co.za>
2024-01-22 15:36:36 +0200

    testing for draw complete

--- test_game.py
+++ test_game.py
@@ -28,14 +28,25 @@ class TestGameFunctions(unittest.TestCase):
         
     def test_compare_tools_draw(self):
         tools = ['rock', 'paper', 'scissors']
+        
         player_tool, comp_tool = "rock", "rock"
         player_score, comp_score = 2, 1
         result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
         self.assertEqual((result), (2,1))
         
+        player_tool, comp_tool = "paper", "paper"
+        player_score, comp_score = 0, 0
+        result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
+        self.assertEqual((result), (0,0))
+        
+        player_tool, comp_tool = "scissors", "scissors"
+        player_score, comp_score = 5, 4
+        result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
+        self.assertEqual((result), (5,4))
+        
     
-    # def test_compare_tools(self):
-    #     pass
+    def test_compare_tools_update_player_score(self):
+        pass
     
     # def test_compare_tools(self):
     #     pass
