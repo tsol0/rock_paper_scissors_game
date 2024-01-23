a76e9f74cc6af85eb3ea82e6df53fa0bf89cc50d
Tsolo Khambule <tskhamb022@student.wethinkcode.co.za>
2024-01-23 13:53:20 +0200

    test for point change for player

--- test_game.py
+++ test_game.py
@@ -11,6 +11,7 @@ class TestGameFunctions(unittest.TestCase):
         score_to_win = 5
         result = g.check_for_winner(player_score, comp_score, score_to_win)
         self.assertEqual(result, False)
+        
     
     def test_check_for_winner_comp_win(self):
         player_score = 2
@@ -19,12 +20,14 @@ class TestGameFunctions(unittest.TestCase):
         result = g.check_for_winner(player_score, comp_score, score_to_win)
         self.assertEqual(result, True)
         
+        
     def test_check_for_winner_player_win(self):
         player_score = 3
         comp_score = 1
         score_to_win = 3
         result = g.check_for_winner(player_score, comp_score, score_to_win)
         self.assertEqual(result, True)
+        
         
     def test_compare_tools_draw(self):
         tools = ['rock', 'paper', 'scissors']
@@ -40,16 +43,30 @@ class TestGameFunctions(unittest.TestCase):
         self.assertEqual((result), (0,0))
         
         player_tool, comp_tool = "scissors", "scissors"
-        player_score, comp_score = 5, 4
+        player_score, comp_score = 5, 2
         result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
-        self.assertEqual((result), (5,4))
+        self.assertEqual((result), (5,2))
         
     
     def test_compare_tools_update_player_score(self):
+        tools = ['rock', 'paper', 'scissors']
+        
+        player_tool, comp_tool = "rock", "scissors"
+        player_score, comp_score = 2, 2
+        result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
+        self.assertEqual((result), (3,2)) 
+        
+        player_score, comp_score = 4, 2
+        result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
+        self.assertEqual((result), (5,2)) 
+        
+        player_score, comp_score = 1, 4
+        result = g.compare_tools(player_tool, comp_tool, tools, comp_score, player_score)
+        self.assertEqual((result), (2,4)) 
+        
+        
+    def test_compare_tools(self):
         pass
-    
-    # def test_compare_tools(self):
-    #     pass
 
 if __name__ == '__main__':
     unittest.main()
